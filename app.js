8acdb6c10d190d54ee113d4ba2ea9937d25ebb3a
edu-bejor <edu-bejor@hotmail.com>
2023-08-10 15:55:06 -0300

    transformando o conteudo do csv para json

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Pasta onde os arquivos serão armazenados
+    cb(null, "uploads/"); // Pasta onde os arquivos serão armazenados
   },
   filename: (req, file, cb) => {
-    const customFileName = 'alunosOriente';
+    const customFileName = "alunosOriente";
     const fileExtension = path.extname(file.originalname);
-    cb(null, customFileName + fileExtension); 
-  }
+    cb(null, customFileName + fileExtension);
+  },
 });
 
 const upload = multer({ storage });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.send('Arquivo enviado com sucesso');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.send("Arquivo enviado com sucesso");
 });
--- index.js
+++ index.js
@@ -5,16 +5,34 @@ const line = readline.createInterface({
   input: fs.createReadStream("uploads/alunosOriente.csv"),
 });
 
-let = firstLine = true;
+let firstLine = true;
+const jsonData = [];
 
 line.on("line", (data) => {
-  let csv = data.split(",");
-
   if (firstLine) {
     firstLine = false;
     return;
   }
-  console.log(
-    `curso: ${csv[0]} - componente: ${csv[1]} - turma: ${csv[2]} - cod: ${csv[3]} - nome: ${csv[4]} - tipo: ${csv[5]} - unidade: ${csv[6]} - escola: ${csv[7]}`
-  );
+
+  const csv = data.split(",");
+  const aluno = {
+    curso: csv[0].replace(/"/g, ""), // Remove aspas e barra a mais
+    componente: csv[1].replace(/"/g, ""),
+    turma: csv[2].replace(/"/g, ""),
+    cod: csv[3].replace(/"/g, ""),
+    nome: csv[4].replace(/"/g, ""),
+    tipo: csv[5].replace(/"/g, ""),
+    unidade: csv[6].replace(/"/g, ""),
+    escola: csv[7].replace(/"/g, ""),
+  };
+
+  jsonData.push(aluno);
+});
+
+line.on("close", () => {
+  const jsonContent = JSON.stringify(jsonData, null, 2);
+
+  fs.writeFileSync("uploads/alunosOriente.json", jsonContent, "utf8");
+
+  console.log("Arquivo JSON gerado com sucesso!");
 });
